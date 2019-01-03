631635c8ebc8e9c5c29b0d86981ff5207d1728ca
Muhammad Hashim <muhammad.hashim@apples-macbook.local>
2019-01-03 13:05:41 +0500

    My new commit work

--- Constrants/ViewController.swift
+++ Constrants/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("New Git Work......")
-        
+      
+          print("New Git Work......")
     }
 
 
