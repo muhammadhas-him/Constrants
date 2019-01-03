76466ea146d359cce5733c4de9e05065d3ce305b
Muhammad Hashim <muhammad.hashim@apples-macbook.local>
2019-01-03 13:03:34 +0500

    Initial Commit

---
+++ Constrants/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Constrants
+//
+//  Created by Muhammad Hashim on 02/01/2019.
+//  Copyright © 2019 Muhammad Hashim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("New Git Work......")
+        
+    }
+
+
+}
+
---
+++ ConstrantsTests/ConstrantsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConstrantsTests.swift
+//  ConstrantsTests
+//
+//  Created by Muhammad Hashim on 02/01/2019.
+//  Copyright © 2019 Muhammad Hashim. All rights reserved.
+//
+
+import XCTest
+@testable import Constrants
+
+class ConstrantsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
